a083a67432fb56535290783f10f95194d9c25a03
Rikki <i@rikki.moe>
2024-03-02 17:48:52 +0800

    Add utils related to crypto

---
+++ utils/crypto/jwt.go
@@ -0,0 +1,42 @@
+package crypto
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type Auth struct {
+	// Secret is the secret key for the JWT.
+	secret []byte
+}
+
+// NewAuth creates a new Auth instance.
+func NewAuth(secret []byte) *Auth {
+	return &Auth{
+		secret: secret,
+	}
+}
+
+// GenerateToken generates a new JWT token.
+func (a *Auth) GenerateToken(sub string, aud string, exp time.Time) (string, error) {
+	claims := jwt.RegisteredClaims{
+		Subject:   sub,
+		Audience:  jwt.ClaimStrings{aud},
+		ExpiresAt: jwt.NewNumericDate(exp),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(a.secret)
+}
+
+// ValidateToken validates a JWT token.
+func (a *Auth) ValidateToken(token string, aud string) (string, error) {
+	tk, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+		return a.secret, nil
+	}, jwt.WithExpirationRequired(), jwt.WithAudience(aud))
+	if err != nil {
+		return "", err
+	}
+
+	return tk.Claims.GetSubject()
+}
--- utils/crypto/random.go
+++ utils/crypto/random.go
@@ -2,16 +2,36 @@ package crypto
 
 import (
 	"crypto/rand"
-	"fmt"
 )
 
-// HexString returns a hex string of nbytes random bytes.
-// This function is cryptographically secure.
-func HexString(nbytes int) string {
+// RandomBytes returns a random byte slice of the given length.
+func RandomBytes(nbytes int) []byte {
 	b := make([]byte, nbytes)
 	_, err := rand.Read(b)
 	if err != nil {
 		panic(err)
 	}
-	return fmt.Sprintf("%x", b)
+	return b
+}
+
+const CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+const LEN_CHARS = len(CHARS)
+// RandomString returns a random string of the given length.
+func RandomString(nchars int) string {
+	b := RandomBytes(nchars)
+	for i, c := range b {
+		b[i] = CHARS[int(c)%LEN_CHARS]
+	}
+	return string(b)
+}
+
+const DIGIT_CHARS = "0123456789"
+const LEN_DIGIT_CHARS = len(DIGIT_CHARS)
+
+func RandomDigits(nchars int) string {
+	b := RandomBytes(nchars)
+	for i, c := range b {
+		b[i] = DIGIT_CHARS[int(c)%LEN_DIGIT_CHARS]
+	}
+	return string(b)
 }
