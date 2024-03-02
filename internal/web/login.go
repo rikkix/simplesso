dc1a978b839780ba541083e3183871b1d2a9dadf
Rikki <i@rikki.moe>
2024-03-02 17:51:17 +0800

    New web server

---
+++ internal/web/login.go
@@ -0,0 +1,91 @@
+package web
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/utils"
+	"github.com/rikkix/simplesso/internal/web/loginreq"
+	"github.com/rikkix/simplesso/utils/url"
+)
+
+func (w *Web) handleIndex(c *fiber.Ctx) error {
+	ssn := w.session(c)
+	redirect := c.Query("redirect", "/")
+
+	if !ssn.Authorized {
+		args := map[string]string{
+			"msg": "Please login to continue.",
+			"redirect": redirect,
+		}
+
+		return c.Redirect(
+			url.AddQueries("/login", args),
+		)
+	}
+
+	return c.SendString("Welcome, "+ssn.Sub+"!")
+}
+
+func (w *Web) handleLogin(c *fiber.Ctx) error {
+	msg := c.Query("msg")
+	redirect := c.Query("redirect")
+
+	return c.Render("login", fiber.Map{
+		"msg": msg,
+		"redirect": redirect,
+	})
+}
+
+func (w *Web) handleLoginPost(c *fiber.Ctx) error {
+	redirect := c.Query("redirect")
+	username := c.FormValue("username")
+	username = strings.ToLower(strings.TrimSpace(username))
+	user := w.config.FindUser(username)
+
+	reqid := ""
+	if user != nil {
+		reqid = w.loginreqdb.NewReq(utils.CopyString(username), 20)
+		go w.tgbot.SendConfirmaion(user.TelegramId, reqid)
+	} else {
+		reqid = loginreq.NewReqID()
+	}
+
+	return c.Render("login_verify", fiber.Map{
+		"redirect": redirect,
+		"reqid": reqid,
+		"username": username,
+	})
+}
+
+func (w *Web) handleVerifyPost(c *fiber.Ctx) error {
+	redirect := c.Query("redirect", "/")
+	reqid := c.FormValue("reqid")
+	code := c.FormValue("code")
+	succ, loginreq := w.loginreqdb.Finish(reqid, code)
+	if !succ {
+		return c.Redirect(url.AddQueries("/login", map[string]string{
+			"msg": "Error verifying code. Please try again.",
+		}))
+	}
+	exp := time.Now().Add(time.Second * time.Duration(loginreq.Dur))
+	fmt.Println("loginreq.Username: ", loginreq.Username)
+	token, err := w.ssnParser.SSOAuther().GenerateToken(loginreq.Username, w.config.Server.SsoHost, exp)
+	if err != nil {
+		w.logger.Error("Error generating token: %s", err)
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	c.Cookie(
+		&fiber.Cookie{
+			Name: "auth_token",
+			Value: token,
+			Expires: exp,
+			// SameSite: "Strict",
+			// HTTPOnly: true,
+		})
+
+	return c.Redirect(redirect)
+}
---
+++ internal/web/web.go
@@ -0,0 +1,88 @@
+package web
+
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
+	"github.com/gofiber/template/html/v2"
+	"github.com/rikkix/simplesso/internal/config"
+	"github.com/rikkix/simplesso/internal/web/loginreq"
+	"github.com/rikkix/simplesso/internal/web/session"
+	"github.com/rikkix/simplesso/internal/web/tg"
+)
+
+
+type Web struct {
+	// Config is the configuration for the web server.
+	config *config.Config
+	// Logger is the logger for the web server.
+	logger log.Logger
+	// Server is the web server.
+	server *fiber.App
+	
+	// ssnParser is the session parser for the web server.
+	ssnParser *session.SessionParser
+	// loginreqdb is the login request database for the web server.
+	loginreqdb *loginreq.MemDB
+	// tgbot is the telegram bot for the web server.
+	tgbot *tg.TG
+
+	// route_registered is a flag to indicate if the routes have been registered.
+	routeRegistered bool
+}
+
+// New creates a new Web instance.
+func New(c *config.Config, l log.Logger, a *fiber.App) *Web {
+	l.Info("Creating new web server...")
+	if a == nil {
+		l.Info("Empty fiber app, creating new one...")
+		a = fiber.New(fiber.Config{
+			Views: html.New(c.Server.WebPath + "layouts", ".html"),
+		})
+	}
+	lrq := loginreq.NewMemDB(30, 5 * time.Minute)
+	tbot, err := tg.New(c.Server.TelegramToken, lrq, l)
+	if err != nil {
+		l.Fatal("Error creating telegram bot: %s", err)
+	}
+	return &Web{
+		config: c,
+		logger: l,
+		server: a,
+		ssnParser: session.NewSessionParser(&c.Server),
+		loginreqdb: lrq,
+		tgbot: tbot,
+		routeRegistered: false,
+	}
+}
+
+// RegisterRoutes registers the routes for the web server.
+func (w *Web) RegisterRoutes() {
+	if w.routeRegistered {
+		return
+	}
+	w.logger.Info("Registering routes...")
+
+	// Register the routes here.
+	w.server.Get("/", w.handleIndex)
+	w.server.Get("/login", w.handleLogin)
+	w.server.Post("/login", w.handleLoginPost)
+	w.server.Post("/verify", w.handleVerifyPost)
+}
+
+// Start starts the web server.
+func (w *Web) Start() {
+	w.logger.Warn("Starting telegram bot...")
+	go w.tgbot.StartPolling()
+
+	w.logger.Warn("Starting web server...")
+	err := w.server.Listen(w.config.Server.ListenAddress)
+	if err != nil {
+		w.logger.Fatal("Error starting web server: %s", err)
+	}
+}
+
+func (w *Web) session(c *fiber.Ctx) *session.Session {
+	return w.ssnParser.Parse(c)
+}
