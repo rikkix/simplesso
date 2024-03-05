e0508f0410bea9c3dbb7d8756c88e22888d18ecb
Rikki <i@rikki.moe>
2024-03-05 18:41:56 +0800

    Add extracthost function

--- utils/url/host.go
+++ utils/url/host.go
@@ -1,7 +1,17 @@
 package url
 
-import "strings"
+import (
+	"strings"
+
+	"github.com/valyala/fasthttp"
+)
 
 func SameHost(host string, expected string) bool {
 	return strings.EqualFold(host, expected)
+}
+
+func ExtractHost(url string) string {
+	uri := fasthttp.URI{}
+	uri.Parse(nil, []byte(url))
+	return string(uri.Host())
 }
