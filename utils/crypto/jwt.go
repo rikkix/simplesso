12ab00fdb322102093804a479d799b67485aa321
Rikki <i@rikki.moe>
2024-03-05 18:42:20 +0800

    return exp time

--- utils/crypto/jwt.go
+++ utils/crypto/jwt.go
@@ -30,13 +30,20 @@ func (a *Auth) GenerateToken(sub string, aud string, exp time.Time) (string, err
 }
 
 // ValidateToken validates a JWT token.
-func (a *Auth) ValidateToken(token string, aud string) (string, error) {
+func (a *Auth) ValidateToken(token string, aud string) (string, time.Time, error) {
 	tk, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
 		return a.secret, nil
 	}, jwt.WithExpirationRequired(), jwt.WithAudience(aud))
 	if err != nil {
-		return "", err
+		return "", time.Time{}, err
 	}
 
-	return tk.Claims.GetSubject()
+	sub, err := tk.Claims.GetSubject()
+	if err != nil {
+		return "", time.Time{}, err
+	}
+
+	exp, err := tk.Claims.GetExpirationTime()
+
+	return sub, exp.Time, err
 }
